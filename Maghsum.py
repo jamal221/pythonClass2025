22216c524fb58f41b27b225dde13c51cb798a025
jamal azizbeigi <jamal.azizbeigi589@gmail.com>
2025-08-04 09:42:02 +0330

    solve new question to find number of divided numbers

--- Maghsum.py
+++ Maghsum.py
@@ -1,6 +1,10 @@
 #مقسوم عليه هاي يک  عدد
-n=int(input("عدد را درج نماييد"))
-
-for i in range(1,n+1):
-    if n%i==0:
-        print(i)
+def maghsum():
+    n=int(input("عدد را درج نماييد"))
+    list1=list()
+    # define variable
+    for i in range(1,n+1):
+        if n%i==0:
+            list1.append(i)
+            #change varibale one step
+    return list1
