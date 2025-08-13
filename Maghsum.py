739d0c7c64678362a33ad733ccf4b0aaf240df5f
Kardorahimy <kardoraahimy@gmail.com>
2025-08-13 10:16:16 +0430

    add prime function with my teacher

--- Maghsum.py
+++ Maghsum.py
@@ -1,6 +1,14 @@
 #مقسوم عليه هاي يک  عدد
-n=int(input("عدد را درج نماييد"))
-
-for i in range(1,n+1):
-    if n%i==0:
-        print(i)
+def magsum(n):
+    #n=int(input("عدد را درج نماييد"))
+    list1=list()
+    for i in range(1,n+1):
+        if n%i==0:
+            list1.append(i)
+    return len(list1)
+def mor(n):
+    if magsum(n)== 2:
+        print("عدد اول است")
+    else:
+        print("عدد مرکب است")
+        
