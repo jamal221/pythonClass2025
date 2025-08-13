7d43c803c71cb827da6667d22ffdbca80786d45b
unknown <shokrolahiarad21@gmail.com>
2025-08-13 09:53:42 +0330

    add new division code

---
+++ priime.py
@@ -0,0 +1,8 @@
+def prime2(n):
+    destination=int=input("adadi bozorgtar az yek ra vared konid")
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            print("morakab")
+    for num in range(2,end+1):
+        if prime (num):
+            print(num)
---
+++ trueDivision.py
@@ -0,0 +1,16 @@
+def maghsum(num):
+    #num = int(input("لطفاً یک عدد وارد کنید: "))
+    divisors =[]
+    # پيمايش مقسوم عليه توسط for
+    for i in range(1, num + 1):
+        if num % i == 0:
+            divisors.append(i)
+          
+    return len(divisors)
+
+#  عدد بررسي جهت اول بودن
+def primeNumber(n):
+    if maghsum(n)==2:
+        print("عدد اول است")
+    else:
+        print("عدد مرکب است")
