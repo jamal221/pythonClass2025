8e0b003ff1f23734b74b91613485f1f6c4675221
taraneh1388 <taranehbanehei@gmail.com>
2025-08-18 09:10:00 +0330

    new commit from school with my teachert

--- Maghsum.py
+++ Maghsum.py
@@ -1,6 +1,8 @@
 #مقسوم عليه هاي يک  عدد
 n=int(input("عدد را درج نماييد"))
-
+list1=list
 for i in range(1,n+1):
-    if n%i==0:
-        print(i)
+       if n%i==0:
+        list1.append
+        print(n)
+print (list1)
