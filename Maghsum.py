d5a4cfd9a660d8c498b78f4d982e7276ec9bbe89
Kardorahimy <kardoraahimy@gmail.com>
2025-08-20 09:59:35 +0430

    maghsum

--- Maghsum.py
+++ Maghsum.py
@@ -2,12 +2,14 @@
 
 
 def maghsum():
+    k=int(input("عدد را درج نماييد"))
     n=int(input("عدد را درج نماييد"))
     list1=list()
-    # define variable
-    for i in range(1,n+1):
-        if n%i==0:
+    list2=list()
+    for i in range(1,n+1)or(1,k+1):
+        if n%i==0 or k%i==0:
             list1.append(i)
+            list2.append(i)
             #change varibale one step
     return list1
-
+    return list2
